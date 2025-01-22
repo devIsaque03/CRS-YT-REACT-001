5c27140513c0f6b2c9dbdb49b7c3b52643e549b3
devIsaque03 <i.09102024@gmail.com>
2025-01-21 21:33:00 -0300

    treinando 001

--- src/pages/Home/Index.jsx
+++ src/pages/Home/Index.jsx
@@ -1,8 +1,25 @@
-function Home() {
-  cont users = [
-    
-  ]
+import './style.css'
+import Trash from '../../assets/trash.svg'
 
+function Home() {
+
+  // simulando um banco de dados
+  const users = [
+    {
+      id: '12342135215',
+      name: 'Rodolfo',
+      age: '33',
+      email: 'rodolfo@gmail.com'
+    },
+    {
+      id: '124512341244',
+      name: 'marta',
+      age: '49',
+      email: 'madalena@gmail.com'
+    }
+  ]
+ 
+  // HTML
   return (
     // Container Principal
     <div className='container'>
@@ -10,29 +27,32 @@ function Home() {
       {/* Formulario de cadastro */}
       <form>
         <h1>Cadastro de Usuários</h1>
-        <input name='nome' type='text'></input>
-        <input name='idade' type='number'></input>
-        <input name='email' type='email'></input>
+        <input placeholder="nome" name='nome' type='text'></input>
+        <input placeholder="idade" name='idade' type='number'></input>
+        <input placeholder="email" name='email' type='email'></input>
         <button type='button'>Cadastrar</button>
       </form>
 
-      {/* Mostruário de cadastros */}
-      <div>
+      {/*Usando JavaScript para percorrer todos usuários e chamar função */}
+      {users.map((user) => (
+        // Mostruário de cadastros
+        <div key={user.id} className='card'>
 
-        {/* Cadastro */}
-        <div>
-           <p>Nome: </p>
-           <p>Idade: </p>
-           <p>Email: </p>
+          {/* Cadastro */}
+          <div>
+            <p>Nome: <span>{user.name}</span></p>
+            <p>Idade: <span>{user.age}</span></p>
+            <p>Email: <span>{user.email}</span></p>
+          </div>
+          <button>
+            <img src={Trash} height=" 30 px"/>
+          </button>
+
         </div>
-        <button>
-          <img src={Trash} />
-        </button>
-
-      </div>
+      ))}
 
     </div>
   );
-}
+};
 
 export default Home;
